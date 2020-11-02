0b4cc61dfe7e78293d1ef92440dfadcdfc20c6ee
Dalton Overlin <overlindalton@gmail.com>
2020-11-02 13:26:30 -0700

    Update content.js

--- extension/core/content/content.js
+++ extension/core/content/content.js
@@ -1,27 +1,33 @@
 var cuizXVI = '1.0'; // Transparency.
 var spanXVI = document.createElement('span');
-spanXVI.innerHTML = '<button id="KTVX1997PLUSONE" style="position:absolute;bottom:0;right:0;color:black;background-color:green;opacity: ' + cuizXVI + ';z-index: 1000;">A</button>';
+spanXVI.innerHTML = '<button id="KTVX1997PLUSONE" style="position:fixed;bottom:0;right:0;color:black;background-color:green;opacity: ' + cuizXVI + ';z-index: 1000;">A</button>';
 document.body.appendChild(spanXVI);
 
 var buttonXVI = document.getElementById("KTVX1997PLUSONE");
 
 buttonXVI.addEventListener("click", function () {
-	if (this.innerText == "A") {
+	if (this.innerText == "A" || this.innerText == "E" || this.innerText == "D") {
 		this.innerText = "B";
 	}
 });
-function handleResponse(message) {
-	console.log(`Message from the background script:  ${message.response}`);
-}
-function handleError(error) {
-	console.log(`Error: ${error}`);
-}
+/*
+A: Not ran yet.
+B: Needs to start a save.
+S: Is currently saving the page.
+D: Process is done.
+E: Error has occured.
+*/
 var intervalXVI = setInterval(function () {
 	var buttonXVITW = document.getElementById("KTVX1997PLUSONE");
 	if (buttonXVITW.innerText == "B") {
-		buttonXVITW.innerText = "A";
-		var sending = browser.runtime.sendMessage({ method: "business.saveTab" }); /* This interracts with the background script. */
-		sending.then(handleResponse, handleError);
-		buttonXVITW.innerText = "A";
+		buttonXVITW.innerText = "S";
+		try {
+		    browser.runtime.sendMessage({ method: "business.saveTab" }); /* This interracts with the background script. */
+		    buttonXVITW.innerText = "D";
+		}
+		catch (Exo){
+		    buttonXVITW.innerText = "E";
+		    console.log("An error occured: "+Exo);
+		}
 	}
 }, 3000);
